d1af7bed0b144d16af4578bf5700f4f067d892dd
Andrej Spilak <andrej.spilak@gmail.com>
2018-12-05 11:09:42 +0100

    Fixes an issue when table name is a reserved keyword (e.g. User) by adding a square brackets around SQL objects

--- EnableTemporalDataOnTable.cs
+++ EnableTemporalDataOnTable.cs
@@ -16,12 +16,12 @@ namespace EFCoreTemporalSupport
             migrationBuilder.Sql($@"
                     IF NOT EXISTS (SELECT * FROM sys.[tables] t INNER JOIN sys.schemas s ON s.schema_id = t.schema_id WHERE t.name = '{tableName}' AND temporal_type = 2 and s.name = '{schemaName}')
                     BEGIN
-                        ALTER TABLE {schemaName}.{tableName}   
+                        ALTER TABLE [{schemaName}].[{tableName}]   
                         ADD  ValidFrom datetime2 (0) GENERATED ALWAYS AS ROW START HIDDEN constraint DF_{tableName}_ValidFrom DEFAULT DATEADD(second, -1, SYSUTCDATETIME())  
                             , ValidTo datetime2 (0)  GENERATED ALWAYS AS ROW END HIDDEN constraint DF_{tableName}_ValidTo DEFAULT '9999.12.31 23:59:59.99'  
                             , PERIOD FOR SYSTEM_TIME (ValidFrom, ValidTo);   
  
-                        ALTER TABLE {schemaName}.{tableName}    
+                        ALTER TABLE [{schemaName}].[{tableName}]
                         SET (SYSTEM_VERSIONING = ON (HISTORY_TABLE = {temporalScheme}.{temporalTableName})); 
                     END
                 ");
@@ -37,15 +37,15 @@ namespace EFCoreTemporalSupport
         {
             var tableName = entityType.Relational().TableName;
             var schemaName = entityType.Relational().Schema ?? "dbo";
-            string alterStatement = $@"ALTER TABLE {tableName} SET (SYSTEM_VERSIONING = OFF);";
+            string alterStatement = $@"ALTER TABLE [{tableName}] SET (SYSTEM_VERSIONING = OFF);";
             migrationBuilder.Sql(alterStatement);
-            alterStatement = $@"ALTER TABLE {tableName} DROP PERIOD FOR SYSTEM_TIME";
+            alterStatement = $@"ALTER TABLE [{tableName}] DROP PERIOD FOR SYSTEM_TIME";
             migrationBuilder.Sql(alterStatement);
-            alterStatement = $@"ALTER TABLE {tableName} DROP CONSTRAINT DF_{tableName}_SysStart, DF_{tableName}_SysEnd";
+            alterStatement = $@"ALTER TABLE [{tableName}] DROP CONSTRAINT DF_{tableName}_SysStart, DF_{tableName}_SysEnd";
             migrationBuilder.Sql(alterStatement);
-            alterStatement = $@"ALTER TABLE {tableName} DROP COLUMN ValidFrom, COLUMN ValidTo";
+            alterStatement = $@"ALTER TABLE [{tableName}] DROP COLUMN ValidFrom, COLUMN ValidTo";
             migrationBuilder.Sql(alterStatement);
-            alterStatement = $@"DROP TABLE {temporalScheme}.{temporalTableName}";
+            alterStatement = $@"DROP TABLE [{temporalScheme}].[{temporalTableName}]";
             migrationBuilder.Sql(alterStatement);
         }
 
